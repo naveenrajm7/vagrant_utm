49b8d0376c7c29c377e239d2ff17cbdfa1741c99
Naveenraj M <naveenrajm97@gmail.com>
2024-07-13 17:56:23 -0600

    Initial commit bundle gem

---
+++ lib/vagrant_utm.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "vagrant_utm/version"
+
+module VagrantUtm
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/vagrant_utm/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module VagrantUtm
+  VERSION = "0.1.0"
+end
