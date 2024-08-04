e8f45bd3f8031025782a194da2ff790e57a4b0fa
Naveenraj M <naveenrajm97@gmail.com>
2024-08-04 16:23:00 -0600

    bugfix: UTM does not have running state yet Caught in unit testing

--- lib/vagrant_utm/cap.rb
+++ lib/vagrant_utm/cap.rb
@@ -5,14 +5,14 @@ module VagrantPlugins
     # Contains all the supported capabilities of the UTM provider.
     module Cap
       # Reads the forwarded ports that currently exist on the machine
-      # itself. This raises an exception if the machine isn't running.
+      # itself. This raises an exception if the machine isn't running (UTM 'started').
       #
       # TODO: This also may not match up with configured forwarded ports, because
       # Vagrant auto port collision fixing may have taken place.
       #
       # @return [Hash<Integer, Integer>] Host => Guest port mappings.
       def self.forwarded_ports(machine)
-        return nil if machine.state.id != :running
+        return nil if machine.state.id != :started
 
         {}.tap do |result|
           machine.provider.driver.read_forwarded_ports.each do |_, _, h, g|
