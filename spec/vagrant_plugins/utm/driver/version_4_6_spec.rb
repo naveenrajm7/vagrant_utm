25d2499ebe3f2860c41e19bf3735fa4b092e1594
Naveenraj M <22456988+naveenrajm7@users.noreply.github.com>
2024-12-03 14:17:30 -0700

    test(driver) : update test for 4.6 driver

--- spec/vagrant_plugins/utm/driver/version_4_6_spec.rb
+++ spec/vagrant_plugins/utm/driver/version_4_6_spec.rb
@@ -2,9 +2,9 @@
 
 require_relative "../base"
 
-RSpec.describe VagrantPlugins::Utm::Driver::Version_4_5 do
+RSpec.describe VagrantPlugins::Utm::Driver::Version_4_6 do
   include_context "utm"
-  let(:utm_version) { "4.5.3" }
+  let(:utm_version) { "4.6.3" }
   subject { VagrantPlugins::Utm::Driver::Meta.new(uuid) }
 
   it_behaves_like "a version 4.x utm driver"
